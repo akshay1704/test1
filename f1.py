90f404f3f11763b4f3404177818eed713ec5fbce
akshay1704 <akajith1704@gmail.com>
2022-09-16 23:25:10 -0400

    change1

--- f1.py
+++ f1.py
@@ -1,3 +1,5 @@
+#using socket
+'''
 import socket
 mysock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org',80))
@@ -13,6 +15,45 @@ while True:
     print(data.decode())
 
 mysock.close()
+'''
+#using request-response
+'''
+from textwrap import indent
+import requests
+import json
 
-    
+url = 'https://api.pushshift.io/reddit/search/submission'
+filter1 = {'subreddit' : 'pushshift', 
+            'size' : 2
+}
+
+response = requests.get(url,filter1)
+
+data = response.json()['data']
+f = open("file1.txt",'w')
+f.write(json.dumps(data,indent=4))
+
+print(data)
+'''
+#using regex and urllib
+'''
+import urllib.request, urllib.parse, urllib.error
+import re
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+for line in fhand:
+    line = line.decode().rstrip()
+    st = re.findall('a.*?d',line)
+    if len(st) == 0 : continue
+    print(st)
+'''
+
+import json
+import requests
+
+response = requests.get('http://35.193.99.196:8983/solr/mycol1/schema/')
+print (response.content)  
+data = response.json()
+f = open("file1.txt",'w')
+f.write(json.dumps(data,indent=4))  
     
